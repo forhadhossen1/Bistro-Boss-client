b164a2ad0d05a0b658298ccee657bb164c8af6d9
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-13 03:06:31 +0600

    complete testmonial with rating

--- src/Pages/Home/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu.jsx
@@ -22,11 +22,14 @@ const PopularMenu = () => {
             </SectionTitle>
             <div className="grid md:grid-cols-2 gap-8 py-12">
                 {
-                    menu.map(item => <MenuItem 
-                    key={item._id}
-                    item={item}
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
                     ></MenuItem>)
                 }
+            </div>
+            <div className="flex justify-center">
+                <button className=" my-5 btn btn-outline border-0 border-b-4 ">View Full Menu</button>
             </div>
         </section>
     );
--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -5,6 +5,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
 
@@ -25,16 +27,21 @@ const Testimonials = () => {
             </SectionTitle>
 
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-               {
-                reviews.map(review => <SwiperSlide
-                key={review._id}
-                >
-                   <div className="m-10 md:m-24">
-                    <p className="text-center">{review.details}</p>
-                    <h3 className="text-2xl text-orange-500 text-center">{review.name}</h3>
-                   </div>
-                </SwiperSlide>)
-               }
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review._id}
+                    >
+                        <div className="flex flex-col items-center m-10 md:m-24 space-y-5">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <p className="text-center">{review.details}</p>
+                            <h3 className="text-2xl text-orange-500 text-center">{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
             </Swiper>
 
         </section>
