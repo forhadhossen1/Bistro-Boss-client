1dfe37415511ed83fa5f58d9e9ac822f17d2dd56
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-17 01:39:31 +0600

    this is final code in before commit

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -2,56 +2,60 @@ import { FaAddressBook, FaCalendar, FaCartShopping, FaEnvelope, FaMoneyBill, FaS
 import { FaHome, FaSearch } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
-const Dashboard = () => (
-    <div className="flex">
+const Dashboard = () => {
+    return (
+        <div className="flex">
 
-        <div className="w-64 min-h-screen bg-orange-400">
-            <ul className="menu p-6 space-y-5 uppercase">
-                <li><NavLink to='/dashboard/userHome'>
-                    <FaHome />  User Home
-                </NavLink></li>
+            <div className="w-64 min-h-screen bg-orange-400">
+                <ul className="menu p-6 space-y-5 uppercase">
+                    <li><NavLink to='/dashboard/userHome'>
+                        <FaHome />  User Home
+                    </NavLink></li>
 
-                <li><NavLink to='/dashboard/reservation'>
-                    <FaCalendar /> Reservation
-                </NavLink></li>
+                    <li><NavLink to='/dashboard/reservation'>
+                        <FaCalendar /> Reservation
+                    </NavLink></li>
 
-                <li><NavLink to='/dashboard/paymentHistory'>
-                    <FaMoneyBill />Payment History
-                </NavLink></li>
+                    <li><NavLink to='/dashboard/paymentHistory'>
+                        <FaMoneyBill />Payment History
+                    </NavLink></li>
 
-                <li><NavLink to='/dashboard/cart'>
-                    <FaCartShopping></FaCartShopping>   My Cart
-                </NavLink></li>
+                    <li><NavLink to='/dashboard/cart'>
+                        <FaCartShopping></FaCartShopping>   My Cart
+                    </NavLink></li>
 
-                <li><NavLink to='/dashboard/review'>
-                    <FaStar />Add Review
-                </NavLink></li>
+                    <li><NavLink to='/dashboard/review'>
+                        <FaStar />Add Review
+                    </NavLink></li>
 
-                <li><NavLink to='/dashboard/boking'>
-                    <FaAddressBook />My Booking
-                </NavLink></li>
+                    <li><NavLink to='/dashboard/boking'>
+                        <FaAddressBook />My Booking
+                    </NavLink></li>
 
-                <div className="divider"></div>
+                    <div className="divider"></div>
 
-                {/* shared nav link........................   */}
+                    {/* shared nav link >>>>>>>>>  */}
 
-                <li><NavLink to='/'>
-                    <FaHome />  Home
-                </NavLink></li>
+                    <li><NavLink to='/'>
+                        <FaHome />  Home
+                    </NavLink></li>
 
-                <li><NavLink to='/menu'>
-                    <FaSearch />  Menu
-                </NavLink></li>
+                    <li><NavLink to='/menu'>
+                        <FaSearch />  Menu
+                    </NavLink></li>
 
-                <li><NavLink to='/contact'>
-                    <FaEnvelope /> Contact
-                </NavLink></li>
-            </ul>
+                    <li><NavLink to='/contact'>
+                        <FaEnvelope/> Contact
+                    </NavLink></li>
+                </ul>
+            </div>
+
+            <div className="flex-1 p-10">
+                <Outlet></Outlet>
+            </div>
+
         </div>
+    );
+};
 
-        <div className="flex-1 p-10">
-            <Outlet></Outlet>
-        </div>
-
-    </div>
-)xport default Dashboard;
+export default Dashboard;
