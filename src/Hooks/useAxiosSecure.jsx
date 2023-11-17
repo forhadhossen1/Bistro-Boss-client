b4b64bb3ae1bd3be656166b802228a2daf73ae40
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-17 18:54:56 +0600

    added useAxionSecure jwt token system for request

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,21 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+  baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
+ // request interceptor to add authorization header for every secure calll to the api 
+  axiosSecure.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    const token = localStorage.getItem('access-token')
+    console.log('request stopped by interceptor', token)
+    config.headers.authorization = `Bearer ${token}`
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  })
   return axiosSecure;
 };
 
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase_config";
+import useAxiosPublic from '../Hooks/useAxiosPublic'
 
 
 export const AuthContext = createContext(null);
@@ -10,8 +11,8 @@ const AuthProviders = ({ children }) => {
 
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic()
 
     // Create User ... 
     const createUser = (email, password) => {
@@ -33,7 +34,7 @@ const AuthProviders = ({ children }) => {
     }
 
     // google signIn 
-    const googleSignIn = () =>{
+    const googleSignIn = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider)
     }
@@ -50,6 +51,20 @@ const AuthProviders = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log("Current User", currentUser);
+            if (currentUser) {
+                //get token and store client
+                const userInfo = {email: currentUser.email};
+                axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token)
+                    }
+                })
+            }
+            else {
+                // todo : remove token
+                localStorage.removeItem('access-token')
+            }
             setLoading(false);
         });
         return () => {
@@ -65,7 +80,7 @@ const AuthProviders = ({ children }) => {
         logOut,
         updateUserProfile,
         googleSignIn,
-        
+
     }
 
     return (
